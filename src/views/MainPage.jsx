3cdf42b3329c1058148d623a10d411f7335df8d9
aydenote <dslgpgh@gmail.com>
2022-10-11 01:21:36 +0900

    reset.css 생성 및 전역 적용

--- src/views/MainPage.jsx
+++ src/views/MainPage.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header.jsx";
+import Product from "../components/Product.jsx";
 
 function MainPage() {
   return (
     <>
       <Header />
+      <Product />
     </>
   );
 }
