df3372749ab5ee709a5f6dfd2d2f57634e2792da
aydenote <dslgpgh@gmail.com>
2022-10-23 23:40:05 +0900

    장바구니 아이템 존재 여부에 따른 컴포넌트 구현

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,32 @@
+import axios from "axios";
 import styles from "./styles/Cart.module.css";
 import circleImg from "../asset/check-circle.svg";
 import CartNoneProduct from "./CartNoneProduct";
 import CartProduct from "./CartProduct";
+import { useState, useEffect } from "react";
 
 function Cart() {
+  let [cartData, setCartData] = useState();
+
+  useEffect(() => {
+    axios({
+      url: `https://openmarket.weniv.co.kr/cart`,
+      method: "get",
+      headers: {
+        withCredentials: true,
+        Authroization:
+          "JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyLCJlbWFpbCI6IiIsInVzZXJuYW1lIjoiYnV5ZXIxIiwiZXhwIjoxNjY3MDUzMzIyfQ.l5bCGUyiYwOdfkj5wuB5Zl4XLs4rbjEjhzecs-yFWOw",
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        setCartData(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <section className={styles.mainContainer}>
       <p className={styles.title}>장바구니</p>
@@ -15,8 +38,7 @@ function Cart() {
         <li>수량</li>
         <li>상품금액</li>
       </ul>
-      {/* 상품이 있는 경우와 없는 경우를 판단하여 컴포넌트를 삼항연산자를 통해 랜더링*/}
-      <CartNoneProduct />
+      {cartData.count === 0 ? <CartNoneProduct /> : <CartProduct />}
     </section>
   );
 }
---
+++ src/components/CartProduct.jsx
@@ -0,0 +1,8 @@
+
+import styles from "./styles/CartProduct.module.css";
+
+function CartProduct() {
+  return;
+}
+
+export default CartProduct;
