1ed419345d0f7f65191c39505c59d24261a7d6d2
aydenote <dslgpgh@gmail.com>
2022-10-30 00:39:51 +0900

    장바구니 페이지 구현

--- src/components/CartProduct.jsx
+++ src/components/CartProduct.jsx
@@ -1,8 +1,12 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import ProductCount from "./ProductCount";
 import styles from "./styles/CartProduct.module.css";
 
 function CartProduct(props) {
+  const [productCount, setProductCount] = useState(0);
+  let [imgUrl, setImgUrl] = useState([]);
+
   function productAxios(productId) {
     return axios.create({
       baseURL: `https://openmarket.weniv.co.kr/products/${productId}`,
@@ -14,17 +18,12 @@ function CartProduct(props) {
   }
 
   async function getProductInfo(cartInfo) {
-    // try {
-    //   const res = await productAxios(productId).get();
-    //   const data = await res.data;
-    //   return data.image;
-    // } catch (error) {
-    //   console.log(error);
-    // }
-    console.log(cartInfo);
-    for (const productInfo of cartInfo) {
-      let imageUrl = await productAxios(productInfo.product_id);
-    }
+    const promises = cartInfo.map(async (item) => {
+      const productInfo = await productAxios(item.product_id).get();
+      return productInfo;
+    });
+
+    setImgUrl(await Promise.all(promises));
   }
 
   useEffect(() => {
@@ -33,8 +32,12 @@ function CartProduct(props) {
 
   return (
     <>
-      {props.cartData.results.map((cartItem) => (
-        <article className={styles.cartItemContainer} key={cartItem.product_id}>
+      {console.log(imgUrl)}
+      {imgUrl.map((cartItem) => (
+        <article
+          className={styles.cartItemContainer}
+          key={cartItem.data.product_id}
+        >
           <input
             className={styles.checkbox}
             type="checkbox"
@@ -42,7 +45,30 @@ function CartProduct(props) {
             id="checkCircle"
           />
           <label className={styles.checkLabel} htmlFor="checkCircle"></label>
-          {/* <img src={getProductInfo(cartItem.product_id)} alt="장바구니 상품" /> */}
+          <img
+            className={styles.productImg}
+            src={cartItem.data.image}
+            alt="장바구니 상품"
+          />
+          <section className={styles.productInfoContainer}>
+            <p className={styles.storeName}>{cartItem.data.store_name}</p>
+            <p className={styles.productName}>{cartItem.data.product_name}</p>
+            <p className={styles.productPrice}>
+              {cartItem.data.price.toLocaleString()}
+            </p>
+            <p className={styles.shipping}>
+              {cartItem.data.shipping_method === "PARCEL"
+                ? "소포배송"
+                : "택배배송"}{" "}
+              / {cartItem.data.shipping_fee === 0 ? "무료배송" : "유료배송"}
+            </p>
+          </section>
+          <section className={styles.productInfoCount}>
+            <ProductCount getCount={productCount} setCount={setProductCount} />
+          </section>
+          <section className={styles.productInfoPrice}>
+            <p>{(cartItem.data.price * productCount).toLocaleString()}</p>
+          </section>
         </article>
       ))}
     </>
