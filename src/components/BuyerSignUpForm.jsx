13c8cd63d3fb751af278b80869cb99f4e2e8885e
aydenote <dslgpgh@gmail.com>
2022-12-08 18:26:54 +0900

    구매회원 가입 페이지 비밀번호 유효성 검사 및 비밀번호 재확인 기능 구현

--- src/components/BuyerSignUpForm.jsx
+++ src/components/BuyerSignUpForm.jsx
@@ -31,22 +31,39 @@ function BuyerSignUpForm() {
     }
   }
 
+  function checkPassword(event) {
+    const reg = /^(?=.*[a-zA-Z])((?=.*\d)|(?=.*\W)).{6,12}$/;
+    const password = event.target.value;
+    if (password.match(reg) == null) {
+      event.target.classList.remove(`${styles.pass}`);
+    } else event.target.classList.add(`${styles.pass}`);
+  }
+
+  function reCheckPassword(event) {
+    const password = document.querySelector(`.${styles.userPwInput}`).value;
+    const confirmPassword = event.target.value;
+
+    if (password != confirmPassword) {
+      event.target.classList.remove(`${styles.pass}`);
+    } else event.target.classList.add(`${styles.pass}`);
+  }
+
   return (
     <form className={styles.buyerSignUpForm} action="">
       <p className={styles.userIdLabel}>아이디</p>
       <div className={styles.userIdContainer}>
-        <input className={styles.userIdInput} type="text" />
+        <input className={styles.userIdInput} type="text" required />
         <button className={styles.confirmIdBtn} onClick={checkDuplicate}>
           중복확인
         </button>
       </div>
       {validText === "" ? <p className={styles.validText}></p> : <p className={styles.validText}>{validText}</p>}
       <p className={styles.userPwLabel}>비밀번호</p>
-      <input className={styles.userPwInput} type="password" />
+      <input className={styles.userPwInput} type="text" placeholder="최소 1개의 숫자 혹은 특수 문자를 포함한 6~12자 영문 비밀번호" onChange={checkPassword} maxLength="12" required />
       <p className={styles.userPwCheckLabel}>비밀번호 재확인</p>
-      <input className={styles.userPwCheckInput} type="password" />
+      <input className={styles.userPwCheckInput} type="text" onChange={reCheckPassword} required />
       <p className={styles.userNameLabel}>이름</p>
-      <input className={styles.userNameInput} type="text" />
+      <input className={styles.userNameInput} type="text" required />
       <p className={styles.userPhoneLabel}>휴대폰번호</p>
       <div className={styles.userPhoneContainer}>
         <select className={styles.userPhoneFirst} name="phone" id="phone">
@@ -63,14 +80,14 @@ function BuyerSignUpForm() {
             017
           </option>
         </select>
-        <input className={styles.userPhoneMiddle} type="number" />
-        <input className={styles.userPhoneLast} type="number" />
+        <input className={styles.userPhoneMiddle} type="number" required />
+        <input className={styles.userPhoneLast} type="number" required />
       </div>
       <p className={styles.userEmailLabel}>이메일</p>
       <div>
-        <input className={styles.userEmailFirst} type="text" />
+        <input className={styles.userEmailFirst} type="text" required />
         <span className={styles.emailAt}>@</span>
-        <input className={styles.userEmailLast} type="text" />
+        <input className={styles.userEmailLast} type="text" required />
       </div>
     </form>
   );
