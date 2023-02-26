922e8c9e93756d62ea0cffb35f4879190c035375
aydenote <dslgpgh@gmail.com>
2023-02-26 21:26:57 +0900

    컴포넌트 파일명 변경으로 경로 수정

--- src/views/MainPage.jsx
+++ src/views/MainPage.jsx
@@ -1,14 +1,14 @@
 import Header from '../components/header/Header.jsx';
-import SlideImg from '../components/slide/MainSlide.jsx';
-import Product from '../components/Product.jsx';
+import MainSlide from '../components/slide/MainSlide.jsx';
+import MainProduct from '../components/MainProduct.jsx';
 import Footer from '../components/footer/Footer.jsx';
 
 function MainPage() {
   return (
     <>
       <Header />
-      <SlideImg />
-      <Product />
+      <MainSlide />
+      <MainProduct />
       <Footer />
     </>
   );
