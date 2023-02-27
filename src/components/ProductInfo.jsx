a60d7c16d18c3cc9da3f0b9768b8e27429a461dc
aydenote <dslgpgh@gmail.com>
2023-02-27 18:30:56 +0900

    스타일 컴포넌트로 변경

--- src/components/ProductInfo.jsx
+++ src/components/ProductInfo.jsx
@@ -1,53 +1,214 @@
-import { useState } from "react";
-import { useLocation } from "react-router-dom";
-import DetailCount from "./DetailCount.jsx";
-import styles from "./styles/ProductInfo.module.css";
+import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import styled from 'styled-components';
+import DetailCount from './DetailCount.jsx';
+import BuyButton from './button/BuyButton';
+import CartButton from './button/CartButton';
 
 function ProductInfo() {
   const data = useLocation().state.data;
   let [productCount, setProductCount] = useState(1);
 
   return (
-    <section className={styles.mainContainer}>
-      <div className={styles.subContainer}>
-        <section className={styles.topContainer}>
-          <section className={styles.productImgContainer}>
+    <MainContainer>
+      <SubContainer>
+        <TopContainer>
+          <ProductImgContainer>
             <img src={data.image} alt="상품 이미지" />
-          </section>
-          <section className={styles.productInfo}>
-            <p className={styles.storeName}>{data.store_name}</p>
-            <p className={styles.productName}>{data.product_name}</p>
-            <p className={styles.productPrice}>{data.price.toLocaleString()}</p>
-            <p className={styles.shipping}>
-              {data.shipping_method === "PARCEL" ? "소포배송" : "택배배송"} / {data.shipping_fee === 0 ? "무료배송" : "유료배송"}
-            </p>
-            <div className={styles.countContainer}>
+          </ProductImgContainer>
+          <ProductInfoContainer>
+            <StoreName>{data.store_name}</StoreName>
+            <ProductName>{data.product_name}</ProductName>
+            <ProductPrice>{data.price.toLocaleString()}</ProductPrice>
+            <Shipping>
+              {data.shipping_method === 'PARCEL' ? '소포배송' : '택배배송'} /{' '}
+              {data.shipping_fee === 0 ? '무료배송' : '유료배송'}
+            </Shipping>
+            <CountContainer>
               <DetailCount setProductCount={setProductCount} productCount={productCount} />
-            </div>
-            <div className={styles.priceContainer}>
-              <p className={styles.total}>총 상품 금액</p>
-              <div className={styles.totalContainer}>
-                <p className={styles.totalCount}>총 수량 {productCount}개</p>
-                <p className={styles.totalPrice}>{(productCount * data.price).toLocaleString()}</p>
-              </div>
-            </div>
-            <div className={styles.buttonContainer}>
-              <button className={styles.buyBtn}>바로 구매</button>
-              <button className={styles.cartBtn}>장바구니</button>
-            </div>
-          </section>
-        </section>
-        <section className={styles.bottomContainer}>
+              <DetailCount />
+            </CountContainer>
+            <PriceContainer>
+              <Total>총 상품 금액</Total>
+              <TotalContainer>
+                <TotalCount>총 수량 {productCount}개</TotalCount>
+                <TotalPrice>{(productCount * data.price).toLocaleString()}</TotalPrice>
+              </TotalContainer>
+            </PriceContainer>
+            <ButtonContainer>
+              <BuyButton />
+              <CartButton />
+            </ButtonContainer>
+          </ProductInfoContainer>
+        </TopContainer>
+        <BottomContainer>
           <ul>
-            <li className={styles.clicked}>버튼</li>
+            <li className="clicked">버튼</li>
             <li>리뷰</li>
             <li>Q&A</li>
             <li>반품/교환정보</li>
           </ul>
-        </section>
-      </div>
-    </section>
+        </BottomContainer>
+      </SubContainer>
+    </MainContainer>
   );
 }
 
 export default ProductInfo;
+
+const MainContainer = styled.section`
+  width: 100%;
+`;
+
+const SubContainer = styled.div`
+  max-width: 80%;
+  margin: auto;
+`;
+
+const TopContainer = styled.section`
+  display: flex;
+  margin-top: 80px;
+`;
+
+const ProductImgContainer = styled.section`
+  flex-grow: 1;
+  flex-shrink: 0;
+  flex-basis: 600px;
+  img {
+    width: 600px;
+    height: 600px;
+    object-fit: fill;
+  }
+`;
+
+const ProductInfoContainer = styled.section`
+  flex-grow: 1;
+  flex-shrink: 0;
+  flex-basis: 600px;
+  margin-left: 50px;
+`;
+
+const StoreName = styled.p`
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 23px;
+  color: #767676;
+`;
+
+const ProductName = styled.p`
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 36px;
+  line-height: 45px;
+  margin: 16px 0px 20px 0px;
+`;
+const ProductPrice = styled.p`
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 36px;
+  line-height: 45px;
+  margin-bottom: 138px;
+  ::after {
+    content: '원';
+    font-family: 'Spoqa Han Sans Neo';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 23px;
+  }
+`;
+const Shipping = styled.p`
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 20px;
+  color: #767676;
+`;
+
+const CountContainer = styled.div`
+  margin: 30px 0px;
+  border-top: 2px solid #c4c4c4;
+  border-bottom: 2px solid #c4c4c4;
+  padding: 30px 0px;
+`;
+
+const PriceContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Total = styled.p`
+  display: flex;
+  align-items: end;
+  margin-right: 50px;
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 23px;
+`;
+
+const TotalContainer = styled.div`
+  display: flex;
+  align-items: end;
+`;
+
+const TotalCount = styled.p`
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 23px;
+  color: #767676;
+  ::after {
+    content: 'ㅣ';
+    font-family: 'Spoqa Han Sans Neo';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 23px;
+    color: #c4c4c4;
+  }
+`;
+
+const TotalPrice = styled.p`
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 36px;
+  color: #21bf48;
+  ::after {
+    content: '원';
+    font-family: 'Spoqa Han Sans Neo';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 23px;
+    color: #21bf48;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  margin-top: 27px;
+`;
+
+const BottomContainer = styled.section`
+  margin: 140px 0 359px 0;
+  li {
+    display: inline-block;
+    width: 25%;
+    height: 60px;
+    text-align: center;
+    line-height: 60px;
+    border-bottom: 6px solid #e0e0e0;
+  }
+  .clicked {
+    border-bottom: 6px solid #21bf48;
+  }
+`;
