1d85bf4b15dd146c7cf6844d5d447bc353ed4010
aydenote <dslgpgh@gmail.com>
2023-02-28 16:11:27 +0900

    로그인 실패 컴포넌트 redux 적용

--- src/components/login/LoginFail.jsx
+++ src/components/login/LoginFail.jsx
@@ -1,6 +1,18 @@
-import styles from "./styles/LoginFail.module.css";
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
 
-function LoginFail({ failText }) {
-  return <p className={styles.failText}>{failText}</p>;
+function LoginFail() {
+  const failText = useSelector(state => state.loginFail);
+
+  return <FailText>{failText}</FailText>;
 }
 export default LoginFail;
+
+const FailText = styled.p`
+  margin-top: 26px;
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  color: red;
+`;
