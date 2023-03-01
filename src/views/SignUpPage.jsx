75ba33a410aa6235b68d4118760d1f146165fef6
aydenote <dslgpgh@gmail.com>
2023-03-01 13:37:22 +0900

    회원가입 페이지 컴포넌트에 불필요한 코드 삭제

--- src/views/SignUpPage.jsx
+++ src/views/SignUpPage.jsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
-import SignUp from "../components/SignUp";
+import SignUp from '../components/SignUp';
 
 function SignUpPage() {
-  const [SignUpType, setSignUpType] = useState("buyer");
-  return <SignUp signUpType={SignUpType} />;
+  return <SignUp />;
 }
 
 export default SignUpPage;
