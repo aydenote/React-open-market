7b379993ebbd49a9db83b1036795aa519379918c
aydenote <dslgpgh@gmail.com>
2023-03-01 19:41:21 +0900

    아이디 유효성에 대한 데이터 추가

--- src/reducers/signupFail.js
+++ src/reducers/signupFail.js
@@ -4,14 +4,17 @@ export const SUCCESS = 'SIGNUPFAIL/SUCCESS';
 export const setFail = text => ({ type: FAIL, text: text });
 export const setPass = text => ({ type: SUCCESS, text: text });
 
-const initalState = '';
+const initalState = {
+  text: '',
+  valid: false,
+};
 
 const signupFail = (state = initalState, action) => {
   switch (action.type) {
     case SUCCESS:
-      return (state = action.text);
+      return { ...state, text: action.text, valid: true };
     case FAIL:
-      return (state = action.text);
+      return { ...state, text: action.text, valid: false };
     default:
       return state;
   }
