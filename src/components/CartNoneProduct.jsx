542d56e767ab1544cade97c7a41d65fe16270db6
aydenote <dslgpgh@gmail.com>
2023-03-03 20:17:57 +0900

    스타일 컴포넌트 적용

--- src/components/CartNoneProduct.jsx
+++ src/components/CartNoneProduct.jsx
@@ -1,12 +1,33 @@
-import styles from "./styles/CartNoneProduct.module.css";
+import styled from 'styled-components';
 
 function CartNoneProduct() {
   return (
-    <section className={styles.container}>
-      <p className={styles.mainText}>장바구니에 담긴 상품이 없습니다.</p>
-      <p className={styles.subText}>원하는 상품을 장바구니에 담아보세요!</p>
-    </section>
+    <Container>
+      <MainText>장바구니에 담긴 상품이 없습니다.</MainText>
+      <SubText>원하는 상품을 장바구니에 담아보세요!</SubText>
+    </Container>
   );
 }
 
 export default CartNoneProduct;
+
+const Container = styled.section`
+  margin-top: 200px;
+  text-align: center;
+`;
+
+const MainText = styled.p`
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 23px;
+`;
+const SubText = styled.p`
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 18px;
+  color: #767676;
+`;
