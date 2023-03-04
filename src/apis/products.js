5a06eea626d1d7112879ac157aae96425571e267
aydenote <dslgpgh@gmail.com>
2023-03-04 20:39:48 +0900

    상품 정보 수신 API  구현

--- src/apis/products.js
+++ src/apis/products.js
@@ -1,8 +1,19 @@
 import axios from 'axios';
+import { getCookie } from '../util/cookie';
 
-export const getProduct = () => {
+export const getProducts = () => {
   return axios({
     url: 'https://openmarket.weniv.co.kr/products',
     method: 'get',
   });
 };
+
+export function getProduct(productId) {
+  return axios({
+    baseURL: `https://openmarket.weniv.co.kr/products/${productId}`,
+    method: 'get',
+    headers: {
+      Authorization: `JWT ${getCookie('Token')}`,
+    },
+  });
+}
