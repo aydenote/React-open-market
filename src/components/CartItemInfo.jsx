fccf00a9f5d04ecd5dbba4777a6e4ceb033f1b44
aydenote <dslgpgh@gmail.com>
2023-03-09 21:41:38 +0900

    장바구니 상품 정보에 대한 컴포넌트 구현

--- src/components/CartItemInfo.jsx
+++ src/components/CartItemInfo.jsx
@@ -1,37 +1,145 @@
-import { useState } from "react";
-import styles from "./styles/CartItemInfo.module.css";
-import ProductCount from "./ProductCount";
-import CartTotalPrice from "./CartTotalPrice";
-import DeleteBtn from "./DeleteBtn";
+import { useEffect, useState } from 'react';
+import DeleteBtn from './DeleteBtn';
+import OrderButton from './button/Order';
+import Decrease from './button/Decrease';
+import Increase from './button/Increase';
+import { getProduct } from '../apis/products';
+import styled from 'styled-components';
 
-function CartItemInfo({ productData }) {
-  let [price, setPrice] = useState(0);
-  let [shipping, setShipping] = useState(0);
+function CartItemInfo({ cartItem }) {
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    async function getProductAPI() {
+      const productData = await getProduct(cartItem.product_id);
+      setProduct(productData);
+    }
+    getProductAPI();
+  }, []);
 
   return (
-    <>
-      {productData.map((cartItem) => (
-        <article className={styles.cartItemContainer} key={cartItem.data.product_id} data-id={cartItem.data.product_id}>
-          <input className={styles.checkbox} type="radio" name="itemSelected" id="itemSelected" />
-          <label className={styles.checkLabel} htmlFor="itemSelected"></label>
-          <img className={styles.productImg} src={cartItem.data.image} alt="장바구니 상품" />
-          <section className={styles.productInfoContainer}>
-            <p className={styles.storeName}>{cartItem.data.store_name}</p>
-            <p className={styles.productName}>{cartItem.data.product_name}</p>
-            <p className={styles.productPrice}>{cartItem.data.price.toLocaleString()}</p>
-            <p className={styles.shipping}>
-              {cartItem.data.shipping_method === "PARCEL" ? "소포배송" : "택배배송"} / {cartItem.data.shipping_fee === 0 ? "무료배송" : "유료배송"}
-            </p>
-          </section>
-          <section className={styles.productInfoCount}>
-            <ProductCount quantity={cartItem.data.quantity} productInfo={cartItem} setPrice={setPrice} price={price} />
-          </section>
-          <DeleteBtn cartInfo={productData} setPrice={setPrice} setShipping={setShipping} />
-        </article>
-      ))}
-      {<CartTotalPrice cartInfo={productData} price={price} shipping={shipping} setShipping={setShipping} />}
-    </>
+    product && (
+      <CartItemContainer key={product.data.product_id}>
+        <input type="radio" name="itemSelected" id="itemSelected" />
+        <ProductImg src={product.data.image} alt="장바구니 상품" />
+        <ProductInfoContainer>
+          <StoreName>{product.data.store_name}</StoreName>
+          <ProductName>{product.data.product_name}</ProductName>
+          <ProductPrice>{product.data.price.toLocaleString()}</ProductPrice>
+          <Shipping>
+            {product.data.shipping_method === 'PARCEL' ? '소포배송' : '택배배송'} /{' '}
+            {product.data.shipping_fee === 0 ? '무료배송' : '유료배송'}
+          </Shipping>
+        </ProductInfoContainer>
+        <ProductInfoCount>
+          <Decrease cart={cartItem} />
+          <ProductCountText>{cartItem.quantity}</ProductCountText>
+          <Increase cart={cartItem} />
+        </ProductInfoCount>
+        <OrderPriceContainer>
+          <SumProductPrice className="test">
+            {(cartItem.quantity * product.data.price).toLocaleString()}
+          </SumProductPrice>
+          <OrderButton />
+        </OrderPriceContainer>
+        <DeleteBtn cartInfo={product} />
+      </CartItemContainer>
+    )
   );
 }
 
 export default CartItemInfo;
+
+const CartItemContainer = styled.article`
+  display: flex;
+  width: 1280px;
+  padding-left: 28px;
+  margin-bottom: 10px;
+  border: 2px solid #e0e0e0;
+  border-radius: 10px;
+  align-items: center;
+`;
+
+const ProductImg = styled.img`
+  width: 160px;
+  height: 160px;
+  margin: 20px 0px 20px 40px;
+  box-shadow: 1px 1px 5px 1px #000000;
+  object-fit: fill;
+  border-radius: 10px;
+`;
+
+const ProductInfoContainer = styled.section`
+  width: 200px;
+  margin-left: 35px;
+`;
+
+const StoreName = styled.p`
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  color: #767676;
+`;
+
+const ProductName = styled.p`
+  margin: 10px 0px;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+`;
+
+const ProductPrice = styled.p`
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+`;
+
+const Shipping = styled.p`
+  margin-top: 40px;
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  color: #767676;
+`;
+
+const ProductCountText = styled.p`
+  flex-grow: 1;
+  flex-shrink: 0;
+  flex-basis: 50px;
+  text-align: center;
+  line-height: 50px;
+`;
+
+const ProductInfoCount = styled.section`
+  display: flex;
+  width: 152px;
+  height: 50px;
+  margin-left: 115px;
+  border: 1px solid #c4c4c4;
+  border-radius: 5px;
+  background: #ffffff;
+`;
+
+const OrderPriceContainer = styled.section`
+  margin-left: 170px;
+`;
+
+const SumProductPrice = styled.p`
+  text-align: center;
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 23px;
+  color: #eb5757;
+  ::after {
+    content: '원';
+  }
+`;
