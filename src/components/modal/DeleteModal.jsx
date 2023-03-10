9dd3bdd9b6902b6cf86eab75c9561f71b2d37533
aydenote <dslgpgh@gmail.com>
2023-03-10 18:38:54 +0900

    장바구니 삭제 모달 컴포넌트 스타일 컴포넌트 적용

--- src/components/modal/DeleteModal.jsx
+++ src/components/modal/DeleteModal.jsx
@@ -1,7 +1,7 @@
-import styles from '../styles/ModalCancel.module.css';
-import deleteImg from '../../asset/icon-delete.svg';
+import deleteSrc from '../../asset/icon-delete.svg';
+import styled from 'styled-components';
 
-function DeleteModal({ cartInfo, setModalOpen, setPrice, setShipping }) {
+function DeleteModal({ cartInfo, setModalOpen, setPrice, setShipping, modalOpen }) {
   function closeModal() {
     setModalOpen(false);
   }
@@ -37,21 +37,75 @@ function DeleteModal({ cartInfo, setModalOpen, setPrice, setShipping }) {
   }
 
   return (
-    <article className={styles.container}>
-      <button className={styles.deleteBtn} onClick={closeModal}>
-        <img src={deleteImg} alt="취소" />
-      </button>
-      <p className={styles.mainText}>상품을 삭제하시겠습니까?</p>
-      <div className={styles.btnContainer}>
-        <button className={styles.cancelBtn} onClick={closeModal}>
-          취소
-        </button>
-        <button className={styles.acceptBtn} onClick={clickAccept}>
-          확인
-        </button>
-      </div>
-    </article>
+    <Container>
+      <DeleteButton onClick={closeModal}>
+        <img src={deleteSrc} alt="취소" />
+      </DeleteButton>
+      <MainText>상품을 삭제하시겠습니까?</MainText>
+      <ButtonContainer>
+        <CancelButton onClick={closeModal}>취소</CancelButton>
+        <AcceptButton onClick={clickAccept}>확인</AcceptButton>
+      </ButtonContainer>
+    </Container>
   );
 }
 
 export default DeleteModal;
+
+const Container = styled.article`
+  width: 300px;
+  height: 200px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #ffffff;
+  border: 1px solid #c4c4c4;
+  border-radius: 8px;
+`;
+
+const DeleteButton = styled.button`
+  display: block;
+  margin: 18px 18px auto auto;
+  padding: 0;
+  border: 0;
+  background-color: transparent;
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const MainText = styled.p`
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 20px;
+  text-align: center;
+`;
+
+const ButtonContainer = styled.div`
+  margin-top: 40px;
+  text-align: center;
+`;
+
+const CancelButton = styled.button`
+  margin-right: 10px;
+  padding: 10px 34.25px;
+  background: #ffffff;
+  border: 1px solid #c4c4c4;
+  border-radius: 5px;
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const AcceptButton = styled.button`
+  padding: 10px 34.25px;
+  background: #21bf48;
+  border: none;
+  border-radius: 5px;
+  :hover {
+    cursor: pointer;
+  }
+`;
