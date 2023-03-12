8e43e10cb8ff57f3bcc8ffe229611ff653ac1a4c
aydenote <dslgpgh@gmail.com>
2023-03-12 21:37:49 +0900

    상품 디테일 정보 API 함수명 변경

--- src/apis/products.js
+++ src/apis/products.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import { getCookie } from '../util/cookie';
 
-export const getProducts = () => {
+export function getProducts() {
   return axios({
     url: 'https://openmarket.weniv.co.kr/products',
     method: 'get',
   });
-};
+}
 
-export async function getProduct(productId) {
+export function getProductDetail(productId) {
   return axios({
     url: `https://openmarket.weniv.co.kr/products/${productId}`,
     method: 'get',
