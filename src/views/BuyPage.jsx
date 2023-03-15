200265f6079bd76bea46d18b1df0cdaf16612fe0
aydenote <dslgpgh@gmail.com>
2023-03-15 15:55:13 +0900

    상품 디테일 페이지 바로 구매 페이지 구현

---
+++ src/views/BuyPage.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Header from '../components/header/Header';
+import BuyInfo from '../components/buy/BuyInfo';
+import ShippingInfo from '../components/buy/ShippingInfo';
+import PaymentMethod from '../components/buy/PaymentMethod';
+import PaymentInfo from '../components/buy/PaymentInfo';
+import styled from 'styled-components';
+
+function BuyPage() {
+  return (
+    <>
+      <Header />
+      <MainContainer>
+        <BuyInfo />
+        <ShippingInfo />
+        <PaymentContainer>
+          <PaymentMethod />
+          <PaymentInfo />
+        </PaymentContainer>
+      </MainContainer>
+    </>
+  );
+}
+
+export default BuyPage;
+
+const MainContainer = styled.section`
+  width: 1280px;
+  margin: auto;
+`;
+
+const PaymentContainer = styled.section`
+  display: flex;
+  margin-top: 70px;
+`;
