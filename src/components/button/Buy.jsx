d568bda60f0493493e20f8d427ffc2bc19d85a2c
aydenote <dslgpgh@gmail.com>
2023-03-15 15:37:30 +0900

    구매 버튼 컴포넌트 클릭 시 페이지 이동 구현

--- src/components/button/Buy.jsx
+++ src/components/button/Buy.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
-function Buy() {
-  return <BuyBtn>바로 구매</BuyBtn>;
+function Buy({ count }) {
+  const data = useLocation().state.data;
+  return (
+    <BuyBtn to="/buy" state={{ productData: data, count: count }}>
+      바로 구매
+    </BuyBtn>
+  );
 }
 
 export default Buy;
 
-const BuyBtn = styled.button`
+const BuyBtn = styled(Link)`
   flex-grow: 2;
   padding: 19px 0px;
   border-radius: 5px;
   border: none;
+  text-align: center;
   color: white;
   background: #21bf48;
   :hover {
--- src/components/button/Order.jsx
+++ src/components/button/Order.jsx
@@ -1,22 +1,18 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 function Order() {
-  return <OrderButton>주문하기</OrderButton>;
+  return <OrderButton to="/buy">주문하기</OrderButton>;
 }
 
 export default Order;
 
-const OrderButton = styled.button`
+const OrderButton = styled(Link)`
   margin-top: 25px;
   padding: 10px 35px;
   outline: none;
   border: none;
   border-radius: 5px;
-  text-align: center;
-  font-family: 'Spoqa Han Sans Neo';
-  font-style: normal;
-  font-weight: 500;
-  font-size: 16px;
   line-height: 20px;
   color: #ffffff;
   background: #21bf48;
