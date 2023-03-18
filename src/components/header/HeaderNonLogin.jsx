29463057f792cfe76ac0e998622148290e798faf
aydenote <dslgpgh@gmail.com>
2023-03-18 21:31:00 +0900

    스타일 컴포넌트 적용

--- src/components/header/HeaderNonLogin.jsx
+++ src/components/header/HeaderNonLogin.jsx
@@ -1,20 +1,64 @@
 import { Link } from 'react-router-dom';
 import CartImg from '../../asset/shopping-cart.svg';
 import UserImg from '../../asset/user.svg';
-import styles from '../styles/HeaderNonLogin.module.css';
+import styled from 'styled-components';
 
 function HeaderNonLogin() {
   return (
     <>
-      <div className={styles.cartContainer}>
-        <img className={styles.cart} src={CartImg} alt="장바구니" />
-      </div>
+      <CartContainer>
+        <CartImage src={CartImg} alt="장바구니" />
+      </CartContainer>
       <Link to="/login">
-        <div className={styles.loginContainer}>
-          <img className={styles.login} src={UserImg} alt="로그인" />
-        </div>
+        <LoginContainer>
+          <LoginImage src={UserImg} alt="로그인" />
+        </LoginContainer>
       </Link>
     </>
   );
 }
 export default HeaderNonLogin;
+
+const CartContainer = styled.div`
+  text-align: center;
+  :hover {
+    cursor: pointer;
+  }
+  ::after {
+    content: '장바구니';
+    display: block;
+    font-family: 'Spoqa Han Sans Neo';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 14px;
+    color: #767676;
+  }
+`;
+
+const CartImage = styled.img`
+  display: block;
+  margin: auto;
+`;
+
+const LoginContainer = styled.div`
+  margin-left: 30px;
+  :hover {
+    cursor: pointer;
+  }
+  ::after {
+    content: '로그인';
+    display: block;
+    font-family: 'Spoqa Han Sans Neo';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 14px;
+    color: #767676;
+  }
+`;
+
+const LoginImage = styled.img`
+  display: block;
+  margin: auto;
+`;
