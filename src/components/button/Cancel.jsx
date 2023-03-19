8331ae1dceb25b7c27b56637f933839a251af183
aydenote <dslgpgh@gmail.com>
2023-03-19 20:52:31 +0900

    삭제 취소 버튼 컴포넌트 구현

---
+++ src/components/button/Cancel.jsx
@@ -0,0 +1,21 @@
+import cancelSrc from '../../asset/icon-delete.svg';
+import styled from 'styled-components';
+
+function Cancel({ setModalOpen }) {
+  function closeModal() {
+    setModalOpen(false);
+  }
+
+  return <CancelButton onClick={closeModal}>취소</CancelButton>;
+}
+
+export default Cancel;
+
+const CancelButton = styled.button`
+  margin-right: 10px;
+  padding: 10px 34.25px;
+  background: #ffffff;
+  border: 1px solid #c4c4c4;
+  border-radius: 5px;
+  cursor: pointer;
+`;
