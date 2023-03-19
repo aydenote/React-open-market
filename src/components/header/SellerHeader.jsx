9946def51a8a1f8d9b1d81d931e603b447a71186
aydenote <dslgpgh@gmail.com>
2023-03-19 15:56:57 +0900

    판매자 센터 헤더 컴포넌트 스타일 컴포넌트 적용

--- src/components/header/SellerHeader.jsx
+++ src/components/header/SellerHeader.jsx
@@ -1,16 +1,37 @@
 import { Link } from 'react-router-dom';
-import LogoImg from '../../asset/Logo-hodu.svg';
-import styles from '../styles/SellerHeader.module.css';
+import LogoSrc from '../../asset/Logo-hodu.svg';
+import styled from 'styled-components';
 
 function SellerHeader() {
   return (
-    <header className={styles.sellerHeader}>
+    <HeaderContainer>
       <Link to="/">
-        <img className={styles.sellerlogoImg} src={LogoImg} alt="로고" />
+        <SellerlogoImg src={LogoSrc} alt="로고" />
       </Link>
-      <p className={styles.sellerCenterText}>판매자 센터</p>
-    </header>
+      <SellerCenterText>판매자 센터</SellerCenterText>
+    </HeaderContainer>
   );
 }
 
 export default SellerHeader;
+
+const HeaderContainer = styled.header`
+  min-width: 1200px;
+  margin: auto;
+  display: flex;
+  padding: 26px 0px;
+  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.1);
+`;
+
+const SellerlogoImg = styled.img`
+  margin-left: 100px;
+`;
+
+const SellerCenterText = styled.p`
+  margin-left: 16px;
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 30px;
+  line-height: 38px;
+`;
