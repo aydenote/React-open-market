89616de6228c081e7abfee7d1cef6f3ca80bef6f
aydenote <dslgpgh@gmail.com>
2023-03-19 12:46:19 +0900

    구매자 header 컴포넌트 스타일 컴포넌트 적용

--- src/components/header/HeaderSeller.jsx
+++ src/components/header/HeaderSeller.jsx
@@ -1,19 +1,40 @@
 import { Link } from 'react-router-dom';
-import BagImg from '../../asset/icon-shopping-bag.svg';
-import styles from '../styles/HeaderSeller.module.css';
 import MyPage from '../MyPage';
+import BagImg from '../../asset/icon-shopping-bag.svg';
+import styled from 'styled-components';
 
 function HeaderSeller() {
   return (
     <>
       <MyPage />
       <Link to="/sellerCenter">
-        <div className={styles.sellerCenter}>
-          <img className={styles.sellerImg} src={BagImg} alt="" />
-          <p className={styles.sellerText}>판매자 센터</p>
-        </div>
+        <SellerCenter>
+          <img src={BagImg} alt="" />
+          <SellerText>판매자 센터</SellerText>
+        </SellerCenter>
       </Link>
     </>
   );
 }
 export default HeaderSeller;
+
+const SellerCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 168px;
+  padding: 8px 0px;
+  margin-left: 30px;
+  background: #21bf48;
+  border-radius: 5px;
+`;
+
+const SellerText = styled.p`
+  margin-left: 12px;
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 23px;
+  color: #ffffff;
+`;
