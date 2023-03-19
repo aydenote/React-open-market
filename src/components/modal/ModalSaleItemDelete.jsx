a3826c8babdf3df510bad76d825c60a090d817f1
aydenote <dslgpgh@gmail.com>
2023-03-19 20:53:16 +0900

    판매자 센터 상품 삭제 모달 컴포넌트 구현

--- src/components/modal/ModalSaleItemDelete.jsx
+++ src/components/modal/ModalSaleItemDelete.jsx
@@ -1,33 +1,57 @@
-import { deleteSaleProduct } from '../../apis/seller';
-import deleteImg from '../../asset/icon-delete.svg';
-import styles from '../styles/ModalSaleItemDelete.module.css';
+import CancelButton from '../button/Cancel';
+import AcceptButton from '../button/Accept';
+import cancelSrc from '../../asset/icon-delete.svg';
+import styled from 'styled-components';
 
 function ModalSaleItemDelete({ setModalOpen }) {
   function closeModal() {
     setModalOpen(false);
   }
 
-  async function clickAccept(event) {
-    const cancelNode = event.target.closest('article').parentNode;
-    cancelNode.parentNode.removeChild(cancelNode);
-    deleteSaleProduct(cancelNode.dataset.id);
-  }
-
   return (
-    <article className={styles.container}>
-      <button className={styles.deleteBtn} onClick={closeModal}>
-        <img src={deleteImg} alt="취소" />
-      </button>
-      <p className={styles.mainText}>상품을 삭제하시겠습니까?</p>
-      <div className={styles.btnContainer}>
-        <button className={styles.cancelBtn} onClick={closeModal}>
-          취소
-        </button>
-        <button className={styles.acceptBtn} onClick={clickAccept}>
-          확인
-        </button>
-      </div>
-    </article>
+    <Container>
+      <CancelImage src={cancelSrc} alt="취소" onClick={closeModal} />
+      <MainText>상품을 삭제하시겠습니까?</MainText>
+      <ButtonContainer>
+        <CancelButton setModalOpen={setModalOpen} />
+        <AcceptButton />
+      </ButtonContainer>
+    </Container>
   );
 }
+
 export default ModalSaleItemDelete;
+
+const Container = styled.article`
+  width: 300px;
+  height: 200px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #ffffff;
+  border: 1px solid #c4c4c4;
+  border-radius: 8px;
+`;
+
+const CancelImage = styled.img`
+  display: block;
+  margin: 18px 18px auto auto;
+  padding: 0;
+  border: 0;
+  background-color: transparent;
+`;
+
+const MainText = styled.p`
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 20px;
+  text-align: center;
+`;
+
+const ButtonContainer = styled.div`
+  margin-top: 40px;
+  text-align: center;
+`;
