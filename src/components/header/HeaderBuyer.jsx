c005ae75d865731836b47caa22dad8f9125796c4
aydenote <dslgpgh@gmail.com>
2023-03-19 12:44:04 +0900

    구매자 header 컴포넌트 스타일 컴포넌트 적용

--- src/components/header/HeaderBuyer.jsx
+++ src/components/header/HeaderBuyer.jsx
@@ -1,18 +1,40 @@
 import { Link } from 'react-router-dom';
-import CartImg from '../../asset/shopping-cart.svg';
-import styles from '../styles/HeaderBuyer.module.css';
 import MyPage from '../MyPage';
+import CartImg from '../../asset/shopping-cart.svg';
+import styled from 'styled-components';
 
 function HeaderBuyer() {
   return (
     <>
       <Link to="/cart">
-        <div className={styles.cartContainer}>
-          <img className={styles.cart} src={CartImg} alt="장바구니" />
-        </div>
+        <CartContainer>
+          <CartImage src={CartImg} alt="장바구니" />
+        </CartContainer>
       </Link>
       <MyPage />
     </>
   );
 }
 export default HeaderBuyer;
+
+const CartContainer = styled.div`
+  text-align: center;
+  :hover {
+    cursor: pointer;
+  }
+  ::after {
+    content: '장바구니';
+    display: block;
+    font-family: 'Spoqa Han Sans Neo';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 14px;
+    color: #767676;
+  }
+`;
+
+const CartImage = styled.img`
+  display: block;
+  margin: auto;
+`;
