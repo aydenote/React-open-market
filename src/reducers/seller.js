15165bc8a4bee2f4550a704f86d751bd9f770239
aydenote <dslgpgh@gmail.com>
2023-03-20 22:08:13 +0900

    판매자 센터 판매 물품에 관한 action, reducer 추가

--- src/reducers/seller.js
+++ src/reducers/seller.js
@@ -1,17 +1,17 @@
-export const GET = 'SALEPRODUCT/GET';
 export const SET = 'SALEPRODUCT/SET';
+export const DELETE = 'SALEPRODUCT/DELETE';
 
-export const getSaleState = saleItem => ({ type: GET, saleItem });
 export const setSaleState = saleItem => ({ type: SET, saleItem });
+export const deleteSaleState = productId => ({ type: DELETE, productId });
 
 const initalState = null;
 
 const sellerProduct = (state = initalState, action) => {
   switch (action.type) {
-    case GET:
-      return state;
     case SET:
       return action.saleItem;
+    case DELETE:
+      return state.filter(saleItem => saleItem.product_id !== action.productId);
     default:
       return state;
   }
