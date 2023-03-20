8a7ca0d4878ab28752361388c921b0d4068f23d2
aydenote <dslgpgh@gmail.com>
2023-03-20 22:25:41 +0900

    마이페이지 드랍다운 컴포넌트 스타일 컴포넌트 적용

--- src/components/MyPageDropDown.jsx
+++ src/components/MyPageDropDown.jsx
@@ -1,26 +1,36 @@
-import { useNavigate } from "react-router-dom";
-import styles from "./styles/MyPageDropDown.module.css";
-import { deleteCookie } from "../util/cookie";
+import MyPage from './button/MyPage';
+import Logout from './button/Logout';
+import styled from 'styled-components';
 
 function MyPageDropDown() {
-  const navigate = useNavigate();
-
-  function clickLogout() {
-    deleteCookie("Token");
-    deleteCookie("UserType");
-    navigate("/");
-    window.location.reload();
-  }
-
   return (
-    <>
-      <section className={styles.container}>
-        <button className={styles.myPageBtn}>마이페이지</button>
-        <button className={styles.logoutBtn} onClick={clickLogout}>
-          로그아웃
-        </button>
-      </section>
-    </>
+    <Container>
+      <MyPage />
+      <Logout />
+    </Container>
   );
 }
 export default MyPageDropDown;
+
+const Container = styled.section`
+  position: absolute;
+  top: 65px;
+  width: 130px;
+  padding: 10px;
+  background-color: #ffffff;
+  border-radius: 8px;
+  z-index: 1;
+  filter: drop-shadow(0px 0px 6px rgba(0, 0, 0, 0.25));
+  ::after {
+    position: absolute;
+    content: '';
+    top: 0;
+    left: 10px;
+    margin-top: -30px;
+    border-top: 20px solid transparent;
+    border-right: 20px solid transparent;
+    border-bottom: 20px solid #ffffff;
+    border-left: 20px solid transparent;
+    z-index: -1;
+  }
+`;
