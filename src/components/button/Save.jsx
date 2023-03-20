1dedfec816fcc285e5271bec8ccd1dda017c541e
aydenote <dslgpgh@gmail.com>
2023-03-20 22:07:06 +0900

    API 함수 분리 및 이미지 데이터 redux 활용

--- src/components/button/Save.jsx
+++ src/components/button/Save.jsx
@@ -1,56 +1,41 @@
-import axios from 'axios';
-import { getCookie } from '../../util/cookie';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { addProduct } from '../../apis/products';
 import styled from 'styled-components';
-import { Navigate } from 'react-router-dom';
 
-function Save({ imgFile }) {
-  async function addProduct() {
+function Save() {
+  const navigate = useNavigate();
+  const imageData = useSelector(state => state.image);
+
+  async function clickSaveProduct() {
+    const formData = new FormData();
     const inputEl = document.querySelectorAll(`input`);
     const productName = inputEl[1].value;
     const productPrice = parseInt(inputEl[2].value.replaceAll(',', ''));
-    const shippingMethod = document.querySelector(`.clicked`).dataset.key;
+    const shippingMethod = document.querySelector('button').disabled ? 'parcel' : 'delivery';
     const shippingFee = parseInt(inputEl[3].value.replaceAll(',', ''));
     const productStock = parseInt(inputEl[4].value.replaceAll(',', ''));
     const productInfo = document.querySelector('textarea').value;
+
+    formData.append('product_name', productName);
+    formData.append('image', imageData);
+    formData.append('price', productPrice);
+    formData.append('shipping_method', shippingMethod === 'parcel' ? 'PARCEL' : 'DELIVERY');
+    formData.append('shipping_fee', shippingFee);
+    formData.append('stock', productStock);
+    formData.append('product_info', productInfo);
+
     try {
-      const res = await addProductAxios(
-        imgFile,
-        productName,
-        productPrice,
-        shippingMethod,
-        shippingFee,
-        productStock,
-        productInfo
-      );
-      if (res.status === 201) {
-        Navigate('/sellerCenter');
+      const addProductRes = await addProduct(formData);
+      if (addProductRes.status === 201) {
+        navigate('/sellerCenter');
       }
     } catch (error) {
       console.error(error);
     }
   }
 
-  function addProductAxios(imgFile, productName, productPrice, shippingMethod, shippingFee, productStock, productInfo) {
-    const formData = new FormData();
-    formData.append('product_name', productName);
-    formData.append('image', imgFile);
-    formData.append('price', productPrice);
-    formData.append('shipping_method', shippingMethod === 'parcel' ? 'PARCEL' : 'DELIVERY ');
-    formData.append('shipping_fee', shippingFee);
-    formData.append('stock', productStock);
-    formData.append('product_info', productInfo);
-    formData.append('token', getCookie('Token'));
-    return axios({
-      url: `https://openmarket.weniv.co.kr/products/`,
-      method: 'post',
-      headers: {
-        Authorization: `JWT ${getCookie('Token')}`,
-      },
-      data: formData,
-    });
-  }
-
-  return <SaveButton onClick={addProduct}>저장하기</SaveButton>;
+  return <SaveButton onClick={clickSaveProduct}>저장하기</SaveButton>;
 }
 
 export default Save;
